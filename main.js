b7c0390ab9c3e95b4e54f61176a857291007a56c
chaitanya08whjr <74759279+chaitanya08whjr@users.noreply.github.com>
2021-10-13 14:38:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+nosex = 0;
+nosey = 0;
+leftwrist = 0;
+rightwrist = 0;
+difference = 0;
+
+function setup(){
+    canvas= createCanvas(500,500);
+    canvas.position(650,150);
+    video = createCapture(VIDEO);
+    video.position(5,150);
+    posenet = ml5.poseNet(video, Modelloaded);
+    posenet.on('pose', gotPoses);
+}
+
+function Modelloaded(){
+    console.log("Modelloaded yay!");
+}
+
+function draw(){
+    background('lightblue');
+    fill('lightgreen');
+    textSize(difference);
+    text('Chaitanya', 50, 400);
+    document.getElementById("span_tag").innerHTML = "height and width of square = " + difference + " px";
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    nosex = results[0].pose.nose.x;
+    nosey = results[0].pose.nose.y;
+    leftwrist = results[0].pose.leftWrist.x;
+    rightwrist = results[0].pose.rightWrist.x;
+    difference = floor(leftwrist-rightwrist);
+    }
+    }
